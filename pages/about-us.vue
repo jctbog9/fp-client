40009657aec903b46cacac42181c423b0d7ed8bb
John Trocchi <jtrocchi1@gmail.com>
2020-02-12 22:10:01 -0500

    migrate about-us page

---
+++ pages/about-us.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="aboutContainer">
+    <h1>About Us</h1>
+    <ul>
+      <li><span class="credit-header">Designed for:</span> Mike Tannenbaum</li>
+      <li>
+        <span class="credit-header">Project Coordinator:</span> Sydney Robinson
+      </li>
+      <li>
+        <span class="credit-header">Designed by Furling Forests Software</span>
+      </li>
+      <li><span class="credit-header">Software Lead:</span> Mike Mathieu</li>
+      <li><span class="credit-header">Engineer:</span> Doug Simon</li>
+      <li><span class="credit-header">Engineer:</span> Lance Sun</li>
+      <li><span class="credit-header">Engineer:</span> John Trocchi</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+#aboutContainer {
+  margin: 10px 16% 0 16%;
+}
+
+ul {
+  margin-top: 15px;
+}
+
+li {
+  display: block;
+  font-family: 'Open Sans', sans-serif;
+}
+
+.credit-header {
+  font-weight: bold;
+}
+</style>
