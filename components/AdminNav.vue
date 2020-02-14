a023a3830d10886429b587e22ca60a360fe8250e
John Trocchi <jtrocchi1@gmail.com>
2020-02-14 17:11:04 -0500

    add notes and start dropdown

---
+++ components/AdminNav.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="header">
+    <div class="left-nav">
+      <nuxt-link class="logo" to="/">
+        <div>Forward</div>
+        <div class="progress">Progress</div>
+      </nuxt-link>
+      <div class="nav-items">
+        <nuxt-link to="/" class="nav-item">
+          Home
+        </nuxt-link>
+        <nuxt-link to="/" class="nav-item">
+          Notes
+        </nuxt-link>
+        <dropdown
+          :links="menuItems"
+        />
+        <nuxt-link to="/blog" class="nav-item">
+          Blog
+        </nuxt-link>
+        <nuxt-link to="/about-us" class="nav-item">
+          About Us
+        </nuxt-link>
+        <nuxt-link to="/upcoming-games" class="nav-item">
+          Upcoming Games
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="nav-right">
+      Logout
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminNav',
+  data() {
+    return {
+      links: [
+        {
+          id: 1,
+          title: 'New Note',
+          slug: 'newnote'
+        },
+        {
+          id: 2,
+          title: 'My Notes',
+          slug: 'mynotes'
+        },
+        {
+          id: 3,
+          title: 'Shared Notes',
+          slug: 'sharednotes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.nav-item {
+  color: #D1D5DA;
+}
+
+.nav-item:hover,
+.nav-item:focus {
+  color: #FFFFFF;
+}
+
+.nav-item:focus {
+  font-weight: bold;
+}
+
+.progress {
+  margin-left: 10px;
+  height: 26px;
+}
+
+.left-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.header {
+  width: 100%;
+  height: 50px;
+  background-color: #013369;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+}
+
+.logo,
+.logo:hover {
+  padding: 5px 10px;
+  font-family: paralucent, sans-serif;
+  font-style: italic;
+  font-weight: 900;
+  font-size: 18px;
+  color: white;
+  line-height: 1em;
+  letter-spacing: 0.05em;
+}
+
+.right-nav {
+  display: flex;
+  flex-direction: row;
+  width: 500px;
+  justify-content: flex-end;
+}
+
+.nav-items {
+  display: flex;
+  flex-direction: row;
+  width: 360px;
+  justify-content: space-evenly;
+}
+</style>
---
+++ components/Dropdown.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="menu">
+    <ul v-if="!isShowsDropdown">
+      <li v-for="link in (links)" class="menu-item">
+        <div class="menu-item__body">
+          <nuxt-link :to="link.url" class="menu-item__link">
+            <span v-html="link.title" />
+          </nuxt-link>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dropdown',
+  props: {
+    links: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  position: absolute;
+  z-index: 999;
+  background: $dark-brown;
+}
+.menu-item {
+  &:first-of-type {
+    padding-top: 8px;
+  }
+  &:last-of-type {
+    margin-bottom: 8px;
+  }
+}
+.menu-item__link {
+  display: inline-block;
+  width: 100%;
+  padding: 8px 16px;
+  font-size: 18px;
+  font-family: cabin;
+  color: RGBA(255, 255, 255, 0.6);
+  &:hover {
+    color: white;
+  }
+}
+.shows-category {
+  color: #666666;
+  margin: 10px 16px 0 16px;
+  font-size: 18px;
+  font-family: oswald;
+}
+</style>
